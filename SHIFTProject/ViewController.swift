80802a76e981dba0f5cd2aa4d0202ead695afcad
Dmitriy <d.karpio@yandex.ru>
2025-07-10 11:46:21 +0300

    initial

---
+++ SHIFTProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SHIFTProject
+//
+//  Created by Дмитрий К on 10.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPurple
+        
+    }
+}
+
